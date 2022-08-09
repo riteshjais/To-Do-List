de387418855899185cd6e1bb3035667d578378bb
Ritesh Jaiswal <riteshjaizz@gmail.com>
2022-08-09 18:40:21 +0530

    Updates

---
+++ precache-manifest.5279066cb4d83a64ea1f8e2fb275eddc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30589a26dfd6c318d8e4eb6eed733d2",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "2db41e42c81d263727d7",
+    "url": "/To-Do-List/static/js/2.d73984dc.chunk.js"
+  },
+  {
+    "revision": "a6feb2e924e7ed6b7745",
+    "url": "/To-Do-List/static/js/main.2b183f3b.chunk.js"
+  },
+  {
+    "revision": "3f9db2d4cf6a2589afcb",
+    "url": "/To-Do-List/static/js/runtime-main.e281b8f3.js"
+  }
+]);
